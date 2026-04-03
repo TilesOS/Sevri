47cda0a2c3360501bf997e53f51569c46c7d50f0
Tyler McClure <tylermcclure11@gmail.com>
2026-04-03 11:26:57 -0400

    changed name everywhere to "sevri"

--- src/lib/email/resend.ts
+++ src/lib/email/resend.ts
@@ -4,7 +4,7 @@ import { getServerEnv } from "@/lib/env";
 const env = getServerEnv();
 const resend = new Resend(env.RESEND_API_KEY);
 
-const DEFAULT_FROM = "Sevri <no-reply@projectforge.app>";
+const DEFAULT_FROM = "Sevri <no-reply@sevri.app>";
 
 export async function sendEmail(to: string, subject: string, html: string) {
   const { error } = await resend.emails.send({
